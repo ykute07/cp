9f39c4c5430f3dfa96179eb1c2e5add3f922e401
ykute07 <92167127+ykute07@users.noreply.github.com>
2021-12-11 11:00:25 +0530

    Create How_Many_Fibs.c

---
+++ How_Many_Fibs.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a = 0, b = 1,c=0,s,f,count = 0;
+    scanf("%d",&s);
+    scanf("%d",&f);
+    while(c < f)
+    {
+        c = a + b;
+        // printf("%d\t",c);
+        a = b;
+        b = c;
+        if(c < s && f > c)
+        {
+            count = count + 1;
+        }
+        // printf("%d",count);
+    }
+    printf("%d",count);
+    return 0;
+}
